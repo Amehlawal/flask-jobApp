43e99d9f5566d6be23c915af6da5ad63d1085bc8
Ameh Lawal <112186253+Amehlawal@users.noreply.github.com>
2023-12-23 07:17:29 -0800

    installed and used sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
